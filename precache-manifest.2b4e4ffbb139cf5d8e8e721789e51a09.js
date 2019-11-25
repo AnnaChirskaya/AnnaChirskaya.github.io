619f679b22e7c35e5b5891ce1fa2593109d515b5
AnnaChirskaya <58178898+AnnaChirskaya@users.noreply.github.com>
2019-11-25 17:33:09 +0300

    Add files via upload

---
+++ precache-manifest.2b4e4ffbb139cf5d8e8e721789e51a09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382733dff23163239da973a1edf62e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "702977033d5d10a1bbbb",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "647e4489cf9149488cf4",
+    "url": "/static/js/2.511dfc50.chunk.js"
+  },
+  {
+    "revision": "702977033d5d10a1bbbb",
+    "url": "/static/js/main.64940f9b.chunk.js"
+  },
+  {
+    "revision": "de2c840306d24257fad5",
+    "url": "/static/js/runtime-main.5b10a5af.js"
+  }
+]);
